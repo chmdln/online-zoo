8fc6d4df56b8200c49c9446df6f54058278727bf
tatyana <tatyanali.official@gmail.com>
2026-03-02 12:55:04 +0500

    fix: resolve paths in header for index.html

--- components/header/header.js
+++ components/header/header.js
@@ -1,4 +1,14 @@
+const scriptSrc = document.currentScript?.src || '';
+const BASE_PATH = scriptSrc.substring(0, scriptSrc.indexOf('/components/header/header.js'));
+
 export function renderHeader() {
+    const paths = [
+        `${BASE_PATH}/index.html`,
+        `${BASE_PATH}/pages/map/index.html`,
+        `${BASE_PATH}/pages/zoos/index.html`,
+        `${BASE_PATH}/pages/contact/index.html`,
+    ]
+
     return `
         <header>
             <h1><img 
@@ -8,10 +18,10 @@ export function renderHeader() {
             ></h1>
             <div class="container-right">
                 <div class="nav-menu">
-                    <a href="/online-zoo/index.html" id="about">About</a>
-                    <a href="/online-zoo/pages/map/index.html" id="map">Map</a>
-                    <a href="/online-zoo/pages/zoos/index.html" id="zoos">Zoos</a>
-                    <a href="/online-zoo/pages/contact/index.html" id="contact-us">Contact Us</a>
+                    <a href="${paths[0]}" id="about">About</a>
+                    <a href="${paths[1]}" id="map">Map</a>
+                    <a href="${paths[2]}" id="zoos">Zoos</a>
+                    <a href="${paths[3]}" id="contact-us">Contact Us</a>
                     <a href="https://www.figma.com/design/lnK11foY8Aoa6oOlDXovVN/Online-ZOO-Project?node-id=44-9749&t=I2rApDlHU6JUq024-0" id="design">Design</a>
                 </div>
                 <div class="social-media">
