15ace4ab01326ab0297335960e03831418042eaa
tatyana <tatyanali.official@gmail.com>
2026-03-02 22:20:13 +0500

    fix: default highlight in header menu

--- pages/contact/script.js
+++ pages/contact/script.js
@@ -1,9 +1,8 @@
 import { renderHeader } from '../../components/header/header.js';
 import { renderFooter } from '../../components/footer/footer.js';
 
-
-
 document.getElementById('header').innerHTML = renderHeader();
 document.getElementById('footer').innerHTML = renderFooter();
-const contact = document.getElementById('contact');
+const contact = document.getElementById('contact-us');
 contact.classList.add('active');
+
