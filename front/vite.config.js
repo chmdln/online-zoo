e38a2a93747f8237ce3ecbc3d7a4cbe357aa96c1
tatyana <tatyanali.official@gmail.com>
2026-04-30 22:31:07 +0500

    fix: vite configs and start command

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        map: './src/pages/map/index.html',
+        zoos: './src/pages/zoos/index.html',
+        contact: './src/pages/contact/index.html',
+        signin: './src/pages/signin/index.html',
+        signup: './src/pages/signup/index.html',
+      }
+    }
+  },
   server: {
     allowedHosts: true
   }
