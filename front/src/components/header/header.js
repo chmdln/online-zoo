b93d20a1b3b7674f97f9530f2c6c996445bbe8aa
tatyana <tatyanali.official@gmail.com>
2026-05-02 16:39:50 +0500

    fix: make nav links absolute and fix paths to images

--- front/src/components/header/header.js
+++ front/src/components/header/header.js
@@ -1,4 +1,4 @@
-const BASE_PATH = new URL('../../assets/images', import.meta.url).href
+const BASE_PATH = '/src/assets'
 
 export function renderHeader() {
     const user = (
@@ -28,33 +28,33 @@ export function renderHeader() {
     return `
         <header>
             <h1><img 
-                src="${BASE_PATH}/assets/images/logo.png"
+                src="${BASE_PATH}/images/logo.png"
                 alt="logo"
                 class="logo-image"
             ></h1>
             <div class="container-right">
                 <div class="nav-menu">
                     <a href="/" id="about">About</a>
-                    <a href="src/pages/map/" id="map">Map</a>
-                    <a href="src/pages/zoos/" id="zoos">Zoos</a>
-                    <a href="src/pages/contact/" id="contact-us">Contact Us</a>
+                    <a href="/src/pages/map/" id="map">Map</a>
+                    <a href="/src/pages/zoos/" id="zoos">Zoos</a>
+                    <a href="/src/pages/contact/" id="contact-us">Contact Us</a>
                     <a href="https://www.figma.com/design/lnK11foY8Aoa6oOlDXovVN/Online-ZOO-Project?node-id=44-9749&t=I2rApDlHU6JUq024-0" id="design">Design</a>
                 </div>
                 <div class="social-media">
                     <a href="https://www.youtube.com/@RSSchool" target="_blank">
-                        <img src="${BASE_PATH}/assets/images/youtube.png" alt="Youtube Logo">
+                        <img src="${BASE_PATH}/images/youtube.png" alt="Youtube Logo">
                     </a>
                     <a href="https://www.instagram.com/rsschool_en/" target="_blank">
-                        <img src="${BASE_PATH}/assets/images/instagram.png" alt="Instagram Logo">
+                        <img src="${BASE_PATH}/images/instagram.png" alt="Instagram Logo">
                     </a>
                     <a href="https://www.facebook.com/rsschoolEN/" target="_blank">
-                        <img src="${BASE_PATH}/assets/images/facebook.png" alt="Facebook Logo">
+                        <img src="${BASE_PATH}/images/facebook.png" alt="Facebook Logo">
                     </a>
                 </div>
             </div>
             <div class="user-container">
                 <div class="user-icon">
-                    <img src="${BASE_PATH}/assets/icons/user.svg" alt="User icon">
+                    <img src="${BASE_PATH}/icons/user.svg" alt="User icon">
                 </div>
                 <div class="user-name">${userName}</div>
                 <div class="user-popup">
@@ -64,13 +64,13 @@ export function renderHeader() {
             <div class="burger-menu">
                 <div class="burger-close-btn">&times</div>
                 <a href="/" id="about">About</a>
-                <a href="/pages/map/" id="map">Map</a>
-                <a href="/pages/zoos/" id="zoos">Zoos</a>
-                <a href="/pages/contact/" id="contact-us">Contact Us</a>
+                <a href="/src/pages/map/" id="map">Map</a>
+                <a href="/src/pages/zoos/" id="zoos">Zoos</a>
+                <a href="/src/pages/contact/" id="contact-us">Contact Us</a>
                 <a href="https://www.figma.com/design/lnK11foY8Aoa6oOlDXovVN/Online-ZOO-Project?node-id=44-9749&t=I2rApDlHU6JUq024-0" id="design">Design</a>
             </div>
             <img 
-                src="${BASE_PATH}/assets/icons/burger.svg" alt="Hamburger icon"
+                src="${BASE_PATH}/icons/burger.svg" alt="Hamburger icon"
                 class="hamburger-icon"
             >
         </header>
@@ -90,11 +90,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         if (e.target.closest('.sign-in-btn')) {
-            window.location.href = `/pages/signin/`;
+            window.location.href = `/src/pages/signin/`;
         } 
 
         if (e.target.closest('.sign-up-btn')) {
-            window.location.href = `/pages/signup/`;
+            window.location.href = `/src/pages/signup/`;
         }
 
         if (e.target.closest('.sign-out-btn')) {
